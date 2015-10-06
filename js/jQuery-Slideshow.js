bff99d0918ecf6b227d46b4f62735f02f9100722
acte-online.com <contact@acte-online.com>
2015-10-06 06:36:42 +0200

    Update jQuery-Slideshow.js

--- js/jQuery-Slideshow.js
+++ js/jQuery-Slideshow.js
@@ -2,26 +2,24 @@ $(document).ready(function () {
 //-----------------------------------------------------------------------------------------
 // SLIDESHOW BANNER
 //-----------------------------------------------------------------------------------------
-	$(document).ready(function() {
-		(function($){  
-		  setInterval(function(){  
-		    //$(".slideshow ul li:first-child").animate({"margin-left": -350}, 800, function(){  
-		    $(".slideshow ul li:first-child").animate({"margin-top": -350}, 1500, function(){
-		        $(this).css("margin-top",0).appendTo(".slideshow ul");  
-		    });  
-		  }, 3500);  
-		})(jQuery);
-	});
-//-----------------------------------------------------------------------------------------
-// SLIDESHOW BANNER IMAGES ALEATOIRE
-//-----------------------------------------------------------------------------------------
-	$(document).ready(function(){
-	    var collection = $("ul.alleatoire li").get();
-	    collection.sort(function() {
-	        return Math.random()*10 > 5 ? 1 : -1;
-	    });
-	    $.each(collection,function(i,el) {
-	        $(el).appendTo( $(el).parent() );
-	    });
-	});
+  $(document).ready(function() {
+  	(function($){  
+  	  setInterval(function(){  
+  	    //$(".slideshow ul li:first-child").animate({"margin-left": -350}, 800, function(){  
+  	    $(".slideshow ul li:first-child").animate({"margin-top": -350}, 1500, function(){
+  	        $(this).css("margin-top",0).appendTo(".slideshow ul");  
+  	    });  
+  	  }, 3500);  
+  	})(jQuery);
+  //-----------------------------------------------------------------------------------------
+  // SLIDESHOW BANNER IMAGES ALEATOIRE
+  //-----------------------------------------------------------------------------------------
+      var collection = $("ul.alleatoire li").get();
+      collection.sort(function() {
+          return Math.random()*10 > 5 ? 1 : -1;
+      });
+      $.each(collection,function(i,el) {
+          $(el).appendTo( $(el).parent() );
+      });
+  });
 });
